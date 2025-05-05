dac75d54dee360184ebe3923a83ecf6bb1f1a28e
Max Efimov <efimov.top.academy@gmail.com>
2025-05-05 11:38:40 +0300

    Добавьте файлы проекта.

---
+++ NetDelegateProject/Examples.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetDelegateProject
+{
+    static class Examples
+    {
+        delegate int Operation(int a, int b);
+        delegate void Message(string message);
+
+        delegate T TOperation<T>(T a, T b);
+
+        delegate T TKOpeartion<T, K>(T a, K b);
+        public static void DelegateWelcomeExample()
+        {
+            Operation operation1 = Summa;
+            Operation operation2 = new Operation(Mult);
+
+            TOperation<int> sum = Summa;
+            TKOpeartion<int, double> tkoper;
+
+            Message? message = SayHello;
+            message("Bobby");
+            Console.WriteLine();
+
+            message += SayGoodby;
+            message("Bobby");
+            Console.WriteLine();
+
+            message -= SayHello;
+            message -= SayGoodby;
+            message?.Invoke("Bobby");
+            Console.WriteLine();
+
+            int Summa(int a, int b) => a + b;
+            int Mult(int a, int b) => a * b;
+            int Calc(int a, int b, Operation o)
+            {
+                return o(a, b);
+            }
+
+            void SayHello(string name)
+            {
+                Console.WriteLine("Hello " + name);
+            }
+
+            void SayGoodby(string name)
+            {
+                Console.WriteLine($"Good by {name}");
+            }
+
+            
+        }
+    }
+}
---
+++ NetDelegateProject/Program.cs
@@ -0,0 +1,63 @@
+﻿Account accaunt = new(1000);
+accaunt.SendAddOk = OkGreenText;
+accaunt.Add(500);
+
+accaunt.SendAddOk = OkMagentaText;
+accaunt.Add(500);
+
+void OkGreenText(string message)
+{
+    var currentForground = Console.ForegroundColor;
+    
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine(message);
+    
+    Console.ForegroundColor = currentForground;
+}
+
+void OkMagentaText(string message)
+{
+    var currentForground = Console.ForegroundColor;
+
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.WriteLine(message);
+
+    Console.ForegroundColor = currentForground;
+}
+
+
+public class Account
+{
+    public delegate void SendMessage(string message);
+
+    public SendMessage SendAddOk { set; get; }
+    public SendMessage SendTakeOk { set; get; }
+    public SendMessage SendTakeError { set; get; }
+
+    int amount;
+    public int Amount { get; private set; }
+
+    public Account(int amount)
+    {
+        this.amount = amount;
+    }
+
+    public void Add(int amount)
+    {
+        this.amount += amount;
+        SendAddOk($"Add {amount} rub. Total: {this.amount}");
+    }
+
+    public void Take(int amount)
+    {
+        if(amount > this.amount)
+        {
+            this.amount -= amount;
+            SendTakeOk($"Take {amount} rub. Total: {this.amount}");
+        }
+        else
+            SendTakeError($"No many. Total: {this.amount}.");
+
+    }
+
+}
